dae1c17900f4508a0acdaa60f6d13f5224a7ca4e
RFozzy688 <pokerfozzy@gmail.com>
2023-05-16 04:40:46 +0300

    описание задачи

---
+++ task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Завдання 1.
+// Файл містить 100000 цілих чисел. Додаток має проаналізувати файл і відобразити статистику по ньому: 
+//1.Кількість додатних чисел. 
+//2.Кількість від'ємних чисел. 
+//3.Кількість двозначних чисел. 
+//4.Кількість п'ятизначних чисел. Крім того, додаток має створити файли з цими числами (додатні, від'ємні і т. д.).
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
