07fa93ebfa2d2440f6f630734b5a7fa79d62ae79
RFozzy688 <pokerfozzy@gmail.com>
2023-05-16 16:16:12 +0300

    описание задачи

---
+++ task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Завдання 2.
+//Користувач вводить з клавіатури слово для пошуку у файлі, шлях до файлу і слово для заміни. 
+//Додаток має змінити усі входження шуканого слова на слово для заміни. Статистику роботи додатку виведіть на екран.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
